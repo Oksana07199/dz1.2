7ad586c0cc3e97764afa2eb7f0eeca02e65a4db2
Oksana Paygildina <kazaikina1997@mail.ru>
2023-12-05 18:10:35 +0300

    sborka padaet RUR

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,7 +1,6 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
@@ -17,5 +16,6 @@ class MobileBankApiTestV1 {
       // Проверки
       .then()
           .statusCode(200);
+
     }
 }
